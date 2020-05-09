fc86cdeec970bd768eb5eb61af63a60759ffa355
5st7 <not404friends@gmail.com>
2020-05-10 01:51:40 +0900

    FirstCommit

---
+++ WPF-DataGrid/Human.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_DataGrid
+{
+    class Human
+    {
+        public enum GenderEnum
+        {
+            None,
+            Men,
+            Women
+        }
+
+        /* 
+         * DataGridに表示するデータ構造を決める
+         * 今回は名前(String),性別(Gender),年齢(int)の3つを追加してみる
+        */
+        public class Person
+        {
+            public string Name { get; set; }
+            //?をつけることでnullを許容するようにする(string型は最初からnull許容できるのでいらない)
+            //ボタンから追加しないなら?マークを外してMainWindow.xamlに書いてあるCanUserAddRows = "false"をCanUserAddRows = "true"にする
+            public GenderEnum? Gender { get; set; }
+            public int? Age { get; set; }
+        }
+    }
+}
---
+++ WPF-DataGrid/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static WpfApp.Human;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<Person> data = new ObservableCollection<Person>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //dataGridとコレクションdataを結び付ける
+            dataGrid.ItemsSource = data;
+
+            //適当にデータを作る
+            Person person = new Person
+            {
+                Name = "田中　太郎",
+                Gender = GenderEnum.Men,
+                Age = 20
+            };
+            //dataにpersonを追加
+            data.Add(person);
+        }
+        //ボタンから追加する方法
+        private void OnClickick(object sender, RoutedEventArgs e)
+        {
+            //空行を追加する方法
+            Person person = new Person
+            {
+                Name = null,
+                Gender = null,
+                Age = null
+            };
+            //dataにpersonを追加
+            data.Add(person);
+        }
+    }
+}
